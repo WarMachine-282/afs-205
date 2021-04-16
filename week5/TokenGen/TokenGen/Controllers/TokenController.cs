71f14c1ca0fa25942f991f36ce7e58fca78b8a69
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2021-04-15 22:06:41 -0700

    docker swarm

---
+++ week5/TokenGen/TokenGen/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokenGen.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new
+            {
+                Guid = Guid.NewGuid().ToString(),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = Environment.MachineName
+            };
+        }
+    }
+}
+
